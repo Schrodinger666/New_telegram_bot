2f8737d891c9b51ae689190231d78287ee2175b2
Nikita T. <130289551+Schrodinger666@users.noreply.github.com>
2024-11-26 18:08:42 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,31 +1,3 @@
-import os
-from dotenv import load_dotenv
+TOKEN = "7978150607:AAHGKI3NRJkrCjYX26vEwJvN1CNqE_9poPg"
 
-# Загружаем переменные окружения из файла .env
-load_dotenv()
-
-# Конфигурация бота
-TOKEN = os.getenv("TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-# Конфигурация базы данных
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", 5432)  # Порт по умолчанию
-
-# Проверка на наличие необходимых переменных окружения
-if not TOKEN:
-    raise ValueError("BOT_TOKEN не установлен в .env файле")
-if not DATABASE_PASSWORD:
-    raise ValueError("DATABASE_PASSWORD не установлен в .env файле")
-if not DB_HOST:
-    raise ValueError("DB_HOST не установлен в .env файле")
-if not CHAT_ID:
-    raise ValueError("CHAT_ID не установлен в .env файле")
-
-# Конфигурация для подключения к базе данных
-DB_CONFIG = {
-    "host": DB_HOST,
-    "port": DB_PORT,
-    "password": DATABASE_PASSWORD
-}
+CHAT_ID = -1002490607817
